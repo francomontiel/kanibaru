d2c38fa3100016abf2c639455f10d047da637d2c
Leonardo <leonardo.velarde.ldvc@gmail.com>
2016-01-29 20:29:33 -0400

    testing boot commit

--- game/states/boot.js
+++ game/states/boot.js
@@ -2,8 +2,10 @@ var States = {};
 
 States.Boot = {
 	preload: function(){
+		this.game.load = this.game.load.image('title', '/assets/images/KANIBARU.png');
 	},
 	create: function(){
+		this.game.background = this.game.add.sprite(this.game.world.centerX - 190, this.game.world.centerY - 40, 'title');
 	},
 	update: function(){
 	}
